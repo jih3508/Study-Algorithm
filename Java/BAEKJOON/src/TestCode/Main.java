cffdd177ecf20f75eeb64fb73501ea3bf125ba10
jih3508 <whdlrgus32@naver.com>
2021-04-11 01:16:14 +0900

    16진수

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -10,10 +10,37 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		BigInteger A = new BigInteger(st.nextToken());
-		BigInteger B = new BigInteger(st.nextToken());
-		System.out.println(A.divide(B));
-		System.out.println(A.remainder(B));
+		String sr = br.readLine();
+		int n = 0;
+		for (int i = sr.length() -1 ; i>=0; i--) {
+			int data;
+			
+			switch (sr.charAt(i)) {
+			case 'A':
+				data = 10;
+				break;
+			case 'B':
+				data = 11;
+				break;
+			case 'C':
+				data = 12;
+				break;
+			case 'D':
+				data = 13;
+				break;
+			case 'E':
+				data = 14;
+				break;
+			case 'F':
+				data = 15;
+				break;
+			default:
+				data = sr.charAt(i) - '0';
+			}
+			
+			n += Math.pow(16, sr.length()-i-1) * data;
+		}
+		System.out.println(n);
+		
 	}
 }
