2fbb6cf57f2fad347a6a8a2ceae8d6d7b39b367d
jih3508 <whdlrgus32@naver.com>
2021-04-12 11:45:16 +0900

    A+B - 2

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER2558.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER2558.java
@@ -1,13 +1,10 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER2558 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine()); 
