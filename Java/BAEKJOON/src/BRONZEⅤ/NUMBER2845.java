11bb460be499505b1701e11693eef51234af4f4b
jih3508 <whdlrgus32@naver.com>
2021-04-13 11:32:45 +0900

    파티가 끝나고 난 뒤

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER2845.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER2845.java
@@ -1,12 +1,11 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER2845 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
