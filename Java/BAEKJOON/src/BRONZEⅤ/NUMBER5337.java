2488baff12ede2cc67bbccb8384f393232f40579
jih3508 <whdlrgus32@naver.com>
2021-04-21 12:34:33 +0900

    웰컴

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER5337.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER5337.java
@@ -1,13 +1,8 @@
-package TestCode;
+package BRONZE¥´;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER5337 {
 	public static void main(String[] args) throws IOException {
 		System.out.print(".  .   .\n"
 				+ "|  | _ | _. _ ._ _  _\n"
