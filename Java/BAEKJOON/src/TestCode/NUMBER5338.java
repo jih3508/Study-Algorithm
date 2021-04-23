97a349b9a1c96ee047404cecc1d29844c497c1a5
jih3508 <whdlrgus32@naver.com>
2021-04-23 15:17:08 +0900

    랜선 자르기

---
+++ Java/BAEKJOON/src/TestCode/NUMBER5338.java
@@ -0,0 +1,13 @@
+package TestCode;
+
+import java.io.IOException;
+
+public class NUMBER5338 {
+	public static void main(String[] args) throws IOException {
+		System.out.print("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}
