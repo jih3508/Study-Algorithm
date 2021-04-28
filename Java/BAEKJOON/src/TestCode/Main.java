5cc91348e30e66ec2238073af55913e5f5242e8e
jih3508 <whdlrgus32@naver.com>
2021-04-28 13:26:59 +0900

    카드 게임

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -9,15 +9,14 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		System.out.print("     /~\\\n"
-				+ "    ( oo|\n"
-				+ "    _\\=/_\n"
-				+ "   /  _  \\\n"
-				+ "  //|/.\\|\\\\\n"
-				+ " ||  \\ /  ||\n"
-				+ "============\n"
-				+ "|          |\n"
-				+ "|          |\n"
-				+ "|          |");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		for(int i = 0; i< 5; i++) {
+			sum += Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum);
+		
 	}
 }
