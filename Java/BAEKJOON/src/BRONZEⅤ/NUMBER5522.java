d4213935a04bbca0bdc4e9e52c44aabd8768f173
jih3508 <whdlrgus32@naver.com>
2021-04-29 11:06:54 +0900

    벌집

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER5522.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER5522.java
@@ -1,5 +1,18 @@
 package BRONZE¥´;
 
-public class NUMBER5522 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class NUMBER5522 {
+	
+		public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		for(int i = 0; i< 5; i++) {
+			sum += Integer.parseInt(br.readLine());
+		}
+		System.out.println(sum);
+	}
 }
---
+++ Python/BAEKJOON/BRONZE Ⅱ/2292 벌집.py
@@ -0,0 +1,7 @@
+n = int(input())
+count = 1
+num = 1
+while n > num:
+    num += count*6
+    count += 1
+print(count)
