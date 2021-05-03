2918c907df83d13ca4d3a0c657d2c671ba5da138
jih3508 <whdlrgus32@naver.com>
2021-05-03 13:23:26 +0900

    Next in line

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER6749.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER6749.java
@@ -1,12 +1,10 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
 
-public class Main {
+public class NUMBER6749 {
 
 	public static void main(String[] args) throws IOException {
 		
@@ -15,5 +13,5 @@ public class Main {
 		int M = Integer.parseInt(br.readLine());
 		
 		System.out.println(M * 2 - Y);
-	}
+	}	
 }
