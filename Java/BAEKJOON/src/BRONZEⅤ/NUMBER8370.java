9b37aa1aab74f6a1bd4c181b0db72077680379f2
jih3508 <whdlrgus32@naver.com>
2021-05-07 09:43:49 +0900

    Plane

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER8370.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER8370.java
@@ -1,12 +1,11 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
+public class NUMBER8370 {
 
 	public static void main(String[] args) throws IOException {
 		
