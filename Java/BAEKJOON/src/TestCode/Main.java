cf4e8cdc1ec2ad54bf5a92614d01ed19f25ed695
jih3508 <whdlrgus32@naver.com>
2021-05-11 17:06:33 +0900

    달팽이는 올라가고 싶다.

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -11,8 +11,10 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		System.out.println(n*(n+1)/2);
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+				
+		System.out.println((a+b)/2);
+		System.out.println((a-b)/2);
 	}
 }
---
+++ Python/BAEKJOON/BRONZE Ⅰ/2869 달팽이는 올라가고 싶다..py
@@ -0,0 +1,2 @@
+A, B, V = map(int, input().split())
+print(1 - (V - A)//(B - A))
