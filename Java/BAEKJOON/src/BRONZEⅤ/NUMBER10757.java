dc25172ee763e213d535a88ad8f2b07199deecdc
jih3508 <whdlrgus32@naver.com>
2021-06-03 00:47:00 +0900

    큰 수 A+B

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER10757.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER10757.java
@@ -1,14 +1,12 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.time.LocalDate;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER10757 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +16,5 @@ public class Main {
 		BigInteger B = new BigInteger(st.nextToken());
 		
 		System.out.println(A.add(B));
-
 	}
 }
