999873ca8c70c2cbf7bff44e46f410d063c708b7
jih3508 <whdlrgus32@naver.com>
2021-06-08 22:14:26 +0900

    ??!

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER10926.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER10926.java
@@ -1,20 +1,16 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.StringTokenizer;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class NUMBER10926 {
+	
+public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String Id = br.readLine();
 		System.out.println(Id + "??!");
-
 	}
 }
