363dd0a7aae8f9c4f5f425009e75fff8148e37ab
jih3508 <whdlrgus32@naver.com>
2021-06-17 22:31:20 +0900

    A+B - 8

--- Java/BAEKJOON/src/BRONZEⅢ/NUMBER11022.java
+++ Java/BAEKJOON/src/BRONZEⅢ/NUMBER11022.java
@@ -1,15 +1,12 @@
-package TestCode;
+package BRONZE¥²;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.time.LocalDate;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER11022 {
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
