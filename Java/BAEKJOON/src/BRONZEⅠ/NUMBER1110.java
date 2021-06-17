5794160819e07dd4688c484f17cd4dedaa10dc94
jih3508 <whdlrgus32@naver.com>
2021-06-17 23:24:05 +0900

    더하기 사이클

--- Java/BAEKJOON/src/BRONZEⅠ/NUMBER1110.java
+++ Java/BAEKJOON/src/BRONZEⅠ/NUMBER1110.java
@@ -1,15 +1,11 @@
-package TestCode;
+package BRONZE¥°;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.StringTokenizer;
 
-public class Main {
-
+public class NUMBER1110 {
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,4 +19,5 @@ public class Main {
 		}
 		System.out.println(count);
 	}
+
 }
