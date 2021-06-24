27daa1248193fe235ccd435429c855af7659bbe2
jih3508 <whdlrgus32@naver.com>
2021-06-24 22:53:56 +0900

    꼬마 정인

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER11382.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER11382.java
@@ -1,23 +1,20 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main {
-	
+public class NUMBER11382 {
+
 	public static void main(String[] args) throws IOException {
-    	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		BigInteger A = new BigInteger(st.nextToken());
 		A = A.add(new BigInteger(st.nextToken()));
 		System.out.println(A.add(new BigInteger(st.nextToken())));
-    }
+
+	}
+
 }
