b2762bf11968f78ff5aa996ccf9746fea8545f5f
jih3508 <whdlrgus32@naver.com>
2021-08-08 23:22:29 +0900

    택시 기하학

--- Java/BAEKJOON/src/BRONZEⅢ/NUMBER3053.java
+++ Java/BAEKJOON/src/BRONZEⅢ/NUMBER3053.java
@@ -1,13 +1,11 @@
-package TestCode;
+package BRONZE¥²;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
-	
+public class NUMBER3053 {
+
 	public static void main(String[] args) throws IOException {
     	
 		int point[][] = new int[3][3];
---
+++ Python/BAEKJOON/BRONZE Ⅲ/3053 택시 기하학.py
@@ -0,0 +1,4 @@
+import math
+n = int(input())
+print(n ** 2 * math.pi)
+print( 2 * n ** 2  )
