2489cc516cf0e869954f304f719f6e13617b591e
jih3508 <whdlrgus32@naver.com>
2021-09-28 00:35:02 +0900

    나는 행복합니다~

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -12,16 +12,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
     	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int a = 0;
-		int b = 1;
-		int c = a + b;
-		for(int i = 2; i<=n; i++) {
-			a = b;
-			b = c;
-			c = a + b;
-		}
-		System.out.println(n == 0 ? a : b);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		System.out.println((k / m) + " " + (k % m));
     }
 	
 }
