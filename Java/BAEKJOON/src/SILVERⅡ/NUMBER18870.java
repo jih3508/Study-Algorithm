66e928abbe91514ebef0891268263c36c28c6fe5
jih3508 <whdlrgus32@naver.com>
2021-10-14 00:23:43 +0900

    좌표 입력

---
+++ Java/BAEKJOON/src/SILVERⅡ/NUMBER18870.java
@@ -0,0 +1,41 @@
+package SILVER¥±;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class NUMBER18870 {
+
+	 public static void main(String[] args) throws IOException{
+	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    	HashMap<Integer, Integer> map = new HashMap<>();
+	    	int n = Integer.parseInt(br.readLine());
+	    	StringTokenizer st = new StringTokenizer(br.readLine());
+	    	int[] array= new int[n];
+	    	int i, num;
+	    	for(i = 0; i < n; i++) {
+	    		num = Integer.parseInt(st.nextToken());
+	    		array[i] = num;
+	    	}
+	    	int[] sort = array.clone();
+	    	Arrays.sort(sort);
+	    	
+	    	int count = 0;
+	    	for(i = 0; i<n; i++) {
+	    		if(!map.containsKey(sort[i])) {
+	    			map.put(sort[i], count);
+	    			count++;
+	    		}
+	    	}
+	    	StringBuilder sb = new StringBuilder();
+	    	for(i = 0; i < n; i++) {
+	    		num = map.get(array[i]);
+	    		sb.append(num).append(" ");
+	    	}
+	    	System.out.println(sb.toString());
+	    	
+	 }
+}
--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -1,6 +1,7 @@
 package TestCode;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -10,20 +11,31 @@ public class Main {
      
      public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	hanoiTop(Integer.parseInt(br.readLine()), 1, 2, 3);
-    	System.out.println(count);
+    	HashMap<Integer, Integer> map = new HashMap<>();
+    	int n = Integer.parseInt(br.readLine());
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int[] array= new int[n];
+    	int i, num;
+    	for(i = 0; i < n; i++) {
+    		num = Integer.parseInt(st.nextToken());
+    		array[i] = num;
+    	}
+    	int[] sort = array.clone();
+    	Arrays.sort(sort);
+    	
+    	int count = 0;
+    	for(i = 0; i<n; i++) {
+    		if(!map.containsKey(sort[i])) {
+    			map.put(sort[i], count);
+    			count++;
+    		}
+    	}
+    	StringBuilder sb = new StringBuilder();
+    	for(i = 0; i < n; i++) {
+    		num = map.get(array[i]);
+    		sb.append(num).append(" ");
+    	}
     	System.out.println(sb.toString());
-     }
-     
-     public static void hanoiTop(int n, int a, int b, int c) {
-    	 if (n == 1) {
-    		 sb.append(a + " " + c).append("\n");
-    		 count +=1;
-    		 return;
-    	 }
-    	 hanoiTop(n - 1, a, c, b);
-    	 sb.append(a + " " + c).append("\n");
-    	 count +=1;
-    	 hanoiTop(n - 1, b, a, c);
-     }
+    	
+     }  
 }
