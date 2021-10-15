1a440bdd59604140ecf59486d9e6e94e659657e8
jih3508 <whdlrgus32@naver.com>
2021-10-16 00:42:46 +0900

    Vera and Outfits

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER15439.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER15439.java
@@ -3,7 +3,6 @@ package BRONZE¥´;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class NUMBER15439 {
 	public static void main(String[] args) throws IOException{
