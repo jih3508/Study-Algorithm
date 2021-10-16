0e854f7d12924cc8809ff0dfb7d36a42fce5b53e
jih3508 <whdlrgus32@naver.com>
2021-10-16 23:40:01 +0900

    조별과제를 하려는데 조장이 사라졌다

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER15727.java
@@ -0,0 +1,14 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER15727 {
+
+	public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+    	System.out.println(n / 5 + (n % 5 == 0 ? 0 : 1) );
+     }  
+}
