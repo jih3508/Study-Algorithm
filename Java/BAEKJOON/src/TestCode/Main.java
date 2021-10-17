1c714738fad67041abe1448f8691d7887d5891b3
jih3508 <whdlrgus32@naver.com>
2021-10-17 23:32:46 +0900

    나는 누구인가

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -10,32 +10,6 @@ public class Main {
      static int count = 0;
      
      public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	HashMap<Integer, Integer> map = new HashMap<>();
-    	int n = Integer.parseInt(br.readLine());
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int[] array= new int[n];
-    	int i, num;
-    	for(i = 0; i < n; i++) {
-    		num = Integer.parseInt(st.nextToken());
-    		array[i] = num;
-    	}
-    	int[] sort = array.clone();
-    	Arrays.sort(sort);
-    	
-    	int count = 0;
-    	for(i = 0; i<n; i++) {
-    		if(!map.containsKey(sort[i])) {
-    			map.put(sort[i], count);
-    			count++;
-    		}
-    	}
-    	StringBuilder sb = new StringBuilder();
-    	for(i = 0; i < n; i++) {
-    		num = map.get(array[i]);
-    		sb.append(num).append(" ");
-    	}
-    	System.out.println(sb.toString());
-    	
+    	System.out.println("I'm Sexy");
      }  
 }
