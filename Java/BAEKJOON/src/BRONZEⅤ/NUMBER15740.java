3beefc0232e04cf793dd26a96eadc1c3f01095d6
jih3508 <whdlrgus32@naver.com>
2021-10-18 23:37:23 +0900

    A+B - 9

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER15740.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER15740.java
@@ -1,18 +1,18 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.*;
-public class Main {
+import java.util.StringTokenizer;
 
-     public static void main(String[] args) throws IOException{
+public class NUMBER15740 {
+
+	public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	BigInteger a = new BigInteger(st.nextToken());
     	BigInteger b = new BigInteger(st.nextToken());
     	System.out.println(a.add(b));
-     }  
+     }
 }
