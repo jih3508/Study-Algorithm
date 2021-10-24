79b79da933de8ea82f3b9a6825fe9016fca4c3bc
jih3508 <whdlrgus32@naver.com>
2021-10-24 23:39:18 +0900

    수학은 체육과목 입니다

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER15894.java
@@ -0,0 +1,15 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class NUMBER15894 {
+	
+	public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BigInteger n = new BigInteger(br.readLine());
+    	System.out.println(n.multiply(BigInteger.valueOf(4)));
+     }  
+}
