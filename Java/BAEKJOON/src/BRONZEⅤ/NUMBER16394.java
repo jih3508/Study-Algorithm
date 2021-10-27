b31818e153619b5790dc7ddb485c7944ef074c6d
jih3508 <whdlrgus32@naver.com>
2021-10-27 11:24:24 +0900

    홍익대학교

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER16394.java
@@ -0,0 +1,13 @@
+package BRONZE¥´;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NUMBER16394 {
+	
+	public static void main(String[] args) throws IOException{
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	System.out.println(n - 1946);
+     }  
+}
