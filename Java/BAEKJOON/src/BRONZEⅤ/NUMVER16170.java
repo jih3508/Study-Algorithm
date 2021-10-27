3a048fcc516e102e8fbbc9b399d49bf1cb2d627b
jih3508 <whdlrgus32@naver.com>
2021-10-27 11:20:39 +0900

    오늘의 날짜는?

--- Java/BAEKJOON/src/BRONZEⅤ/NUMVER16170.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMVER16170.java
@@ -4,17 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.StringTokenizer;
 
 public class NUMVER16170 {
 	
-	public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	BigInteger A = new BigInteger(st.nextToken());
-    	BigInteger B = new BigInteger(st.nextToken());
-    	BigInteger add = A.add(B);
-    	BigInteger sub = A.subtract(B);
-    	System.out.println(add.multiply(sub));
-     }  
+	 public static void main(String[] args) throws IOException{
+	    	LocalDateTime now = LocalDateTime.now();
+	    	now.minusHours(9);
+	    	System.out.println(now.getYear());
+	    	System.out.println(now.getMonthValue());
+	    	System.out.println(now.getDayOfMonth());
+	     }  
 }
