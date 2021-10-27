2bac6a46a83241963183641128ee4be008e235e9
jih3508 <whdlrgus32@naver.com>
2021-10-27 10:58:21 +0900

    이상한 기호

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER15964.java
@@ -0,0 +1,21 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class NUMBER15964 {
+	
+	public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	BigInteger A = new BigInteger(st.nextToken());
+    	BigInteger B = new BigInteger(st.nextToken());
+    	BigInteger add = A.add(B);
+    	BigInteger sub = A.subtract(B);
+    	System.out.println(add.multiply(sub));
+     }  
+
+}
