88ab99d9a971a0e48836fbe76106bc3b7ef74520
jih3508 <whdlrgus32@naver.com>
2021-10-28 13:17:50 +0900

    18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER18108.java
@@ -0,0 +1,15 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER18108 {
+	
+public static void main(String[] args) throws IOException{
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+     	System.out.println(n - 543);
+     }  
+}
