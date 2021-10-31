5f51965e0312f38f3bc0bcf413027a23109fac36
jih3508 <whdlrgus32@naver.com>
2021-10-31 23:30:08 +0900

    Bottle Return

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER21300.java
@@ -0,0 +1,21 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NUMBER21300 {
+	
+	public static void main(String[] args) throws IOException{
+
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int n = 0;
+    	for(int i = 0; i< 6; i++) {
+    		n += Integer.parseInt(st.nextToken()) * 5;
+    	}
+    	  	
+    	System.out.println(n);
+     } 
+}
