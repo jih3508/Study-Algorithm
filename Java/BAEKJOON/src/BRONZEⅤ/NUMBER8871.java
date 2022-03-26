a7a3478718adccf90e1bd9e55b17a9111555c484
jih3508 <whdlrgus32@naver.com>
2022-03-26 10:11:27 +0900

    Zadanie próbne 2

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER8871.java
@@ -0,0 +1,17 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER8871 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println((2*(n+1)) + " " + (3 *(n+1)));
+
+	}
+
+}
