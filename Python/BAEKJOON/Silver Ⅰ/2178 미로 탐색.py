2612c56fde31232734d3aa2d0d9b6ae7091f5703
jih3508 <whdlrgus32@naver.com>
2022-06-27 01:55:37 +0900

    미로 탐색

---
+++ Python/BAEKJOON/Silver Ⅰ/2178 미로 탐색.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+matrix = [list(map(int, input()[:m])) for _ in range(n)]
+
+
+#방향
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+queue = deque([[0, 0]]) # 출발 지점
+
+while queue:
+    y, x = queue.popleft()
+    value = matrix[y][x]
+    for i in range(4):
+        fx = x + dx[i]
+        fy = y + dy[i]
+        
+        if (0 <= fx < m) and (0 <= fy < n) and (matrix[fy][fx] == 1):
+            queue.append([fy, fx])
+            matrix[fy][fx] = value + 1
+
+print(matrix[n-1][m-1])
