06c46b8e391a2ab80f2808b585fdc24c15155773
jih3508 <whdlrgus32@naver.com>
2022-08-10 00:28:05 +0900

    새씩

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER25083.java
@@ -0,0 +1,16 @@
+package BRONZE¥´;
+
+public class NUMBER25083 {
+	
+	public static void main(String[] args){
+
+    	System.out.println("         ,r'\"7\n"
+    			+ "r`-_   ,'  ,/\n"
+    			+ " \\. \". L_r'\n"
+    			+ "   `~\\/\n"
+    			+ "      |\n"
+    			+ "      |");
+    	  	
+     }
+
+}
