0f9df201933a478956f260a4ab1fc952a2f85979
jih3508 <whdlrgus32@naver.com>
2022-08-13 02:06:45 +0900

    영수증

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER25304.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER25304.java
@@ -1,5 +1,24 @@
 package BRONZE¥´;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
 public class NUMBER25304 {
 
+	public static void main(String[] args) throws IOException{
+
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BigInteger X = new BigInteger(br.readLine());
+    	int N =  Integer.valueOf(br.readLine());
+    	for(int i = 0; i < N; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		BigInteger a = new BigInteger(st.nextToken());
+    		a = a.multiply(new BigInteger(st.nextToken()));
+    		X = X.subtract(a);
+    	}
+    	System.out.println("0".equals(X.toString())? "Yes" : "No");
+     }
 }
