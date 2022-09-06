89a0979bb0e4ae7251ed56e9f05dd3478241f5a4
jih3508 <jih3508@ibizsoftware.net>
2022-09-06 11:15:43 +0900

    약수

---
+++ Java/BAEKJOON/src/BRONZEⅠ/NUMBER1037.java
@@ -0,0 +1,28 @@
+package BRONZE¥°;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NUMBER1037 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int n = Integer.parseInt(br.readLine());
+	    
+		int[] nums = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max_num = Arrays.stream(nums).max().getAsInt();
+		int min_num = Arrays.stream(nums).min().getAsInt();
+		
+		System.out.println(max_num * min_num);
+	}	
+}
