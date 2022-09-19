922cd0d718bbfc37684a574b7538b5b206558825
jih3508 <jih3508@ibizsoftware.net>
2022-09-19 13:56:27 +0900

    단어 길이 재기

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER2743.java
@@ -0,0 +1,14 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER2743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+		
+	}
+}
