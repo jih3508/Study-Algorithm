3b5c9fdb343382b70ffb8f5fd0506e1a05ec68d0
jih3508 <whdlrgus32@naver.com>
2022-10-10 03:50:44 +0900

    Shares

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER3733.java
@@ -0,0 +1,18 @@
+package BRONZE¥´;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NUMBER3733 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N, S;
+		while(sc.hasNext()) {
+			N = sc.nextInt();
+			S = sc.nextInt();
+			System.out.println(S / (N + 1));
+		}
+		
+	}
+}
