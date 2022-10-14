8fda239abfbf06b93f8f408727cca280b80888d6
jih3508 <jih3508@ibizsoftware.net>
2022-10-14 17:37:25 +0900

    크냐?

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER4101.java
@@ -0,0 +1,29 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NUMBER4101 {
+
+public static void main(String[] args) throws IOException {
+		
+		int x, y;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while(true) {	
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			if(x ==0 && y == 0) break;
+			if (x > y) {
+				sb.append("Yes\n");
+			}else {
+				sb.append("No\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
---
+++ Python/BAEKJOON/BRONZE Ⅴ/4101 크냐.py
@@ -0,0 +1,4 @@
+while(True):
+    x, y = map(int, input().split())
+    if(x == 0 and y == 0): break;
+    print(('No', 'Yes')[x > y])
