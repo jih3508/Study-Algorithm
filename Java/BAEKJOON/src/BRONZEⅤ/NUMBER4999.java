87239d6dc56a6d1435752e3b69f5bb84e49fcbcf
jih3508 <whdlrgus32@naver.com>
2022-10-22 00:35:45 +0900

    아!

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER4999.java
@@ -0,0 +1,17 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER4999 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String string1 = br.readLine();
+		String string2 = br.readLine();
+		
+		System.out.println(string1.lastIndexOf('a') >= string2.lastIndexOf('a') ? "go" : "no" );
+		
+	}
+}
---
+++ Python/BAEKJOON/BRONZE Ⅴ/4999 아!.py
@@ -0,0 +1,3 @@
+string1 = input()
+string2 = input()
+print("go" if string1.index('h') >= string2.index('h') else "no")
