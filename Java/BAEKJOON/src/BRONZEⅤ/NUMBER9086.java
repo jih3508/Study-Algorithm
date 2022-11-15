0c484c4e56f7d3c1ef0c08bbda095e23537d3b16
jih3508 <whdlrgus32@naver.com>
2022-11-16 00:29:44 +0900

    9086

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER9086.java
@@ -0,0 +1,19 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER9086 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String string;
+		while(N-- > 0) {
+			string = br.readLine();
+			System.out.println(string.charAt(0) + "" + string.charAt(string.length() - 1));
+		}
+		
+	}
+}
---
+++ Python/BAEKJOON/BRONZE Ⅴ/9086 문자열.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    string = input()
+    print(string[0] + string[-1])
