f14a5fa7ba595c69489a6a1684facf4ccbe526a4
jih3508 <whdlrgus32@naver.com>
2022-11-18 01:30:52 +0900

    팬들에게 둘러싸인 홍준

--- Java/BAEKJOON/src/BRONZEⅤ/NUMBER14581.java
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER14581.java
@@ -1,11 +1,11 @@
-package TestCode;
+package BRONZE¥´;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-	
+public class NUMBER14581 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
@@ -13,5 +13,4 @@ public class Main {
 		System.out.println(":fan::"+ str +"::fan:");
 		System.out.println(":fan::fan::fan:");
 	}
-	
 }
