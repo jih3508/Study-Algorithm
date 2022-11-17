a0e17d347f64bcc9e0f24aca24bc8e6c9518312b
jih3508 <whdlrgus32@naver.com>
2022-11-18 01:34:56 +0900

    Revert "팬들에게 둘러싸인 홍준"
    This reverts commit f14a5fa7ba595c69489a6a1684facf4ccbe526a4.

--- Java/BAEKJOON/src/TestCode/Main.java
+++ Java/BAEKJOON/src/TestCode/Main.java
@@ -3,15 +3,35 @@ package TestCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		System.out.println(":fan::fan::fan:");
-		System.out.println(":fan::"+ str +"::fan:");
-		System.out.println(":fan::fan::fan:");
+		StringTokenizer st;
+		int size = Integer.parseInt(br.readLine());
+		
+		int[][] house = new int[size][3];
+		
+		
+		for(int i =0; i < size; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				house[i][j] = Integer.parseInt(st.nextToken());
+			}			
+		}
+		
+		for(int i = 1; i < size; i++) {
+			house[i][0] += Math.min(house[i-1][1], house[i-1][2]);
+			house[i][1] += Math.min(house[i-1][0], house[i-1][2]);
+			house[i][2] += Math.min(house[i-1][1], house[i-1][0]);
+		}
+		
+		System.out.println(Arrays.stream(house[size-1]).min().getAsInt());
 	}
 	
 }
