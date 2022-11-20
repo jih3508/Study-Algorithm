13aa63bc840ebd2e0f69150b95f6bf0788d68be1
jih3508 <whdlrgus32@naver.com>
2022-11-21 00:30:41 +0900

    SciComLove

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER21598.java
@@ -0,0 +1,17 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER21598 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		while(N-- >0) {
+			System.out.println("SciComLove");
+		}
+	}
+	
+}
