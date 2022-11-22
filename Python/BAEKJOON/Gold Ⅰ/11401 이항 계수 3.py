2fae05405605098282b3ad89a208626e5b3a4df3
jih3508 <jih3508@ibizsoftware.net>
2022-11-22 17:13:23 +0900

    코드 수정

--- Python/BAEKJOON/Gold Ⅰ/11401 이항 계수 3.py
+++ Python/BAEKJOON/Gold Ⅰ/11401 이항 계수 3.py
@@ -4,17 +4,7 @@ def factorial(n):
         result = result * n % P
         n -= 1
     return result
-
-# base: 밑, expo: 지수
-def FIT(base, expo):
-    if expo == 1: return base % P
     
-    result = FIT(base, expo //2)
-    result = result * result % P
-    if(expo % 2 == 1): return result * base % P
-    else: return result
-    
-
 P = 1000000007
 N, K = map(int, input().split())
-print(factorial(N) * FIT(factorial(N - K) * factorial(K) % P, P - 2) % P)
+print(factorial(N) * pow(factorial(N - K) * factorial(K) % P, P - 2, P) % P)
