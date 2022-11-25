19783c59e3eff968ecd4651a9ac09400fa493e51
jih3508 <jih3508@ibizsoftware.net>
2022-11-25 10:20:25 +0900

    뒤집기 코드 수정

--- Java/BAEKJOON/src/SILVERⅤ/NUMBER1439.java
+++ Java/BAEKJOON/src/SILVERⅤ/NUMBER1439.java
@@ -8,18 +8,14 @@ import java.util.Arrays;
 public class NUMBER1439 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		String S = br.readLine();
-		String result = S.substring(0, 1);
+		int[] count = {0, 0};
+		count[Integer.parseInt(S.substring(0, 1))]++;
 		for(int i = 1; i < S.length(); i++) {
-			if(S.charAt(i) != result.charAt(result.length() - 1)) {
-				result += S.substring(i, i + 1);
+			if(S.charAt(i - 1) != S.charAt(i)) {
+				count[Integer.parseInt(S.substring(i, i+1))]++;
 			}
-		}
-		
-		int[] count = new int[2];
-		for(int i = 0; i < result.length(); i++) {
-			count[Integer.parseInt(result.substring(i, i + 1))]++;
 		}
 		System.out.println(Arrays.stream(count).min().getAsInt());
 		
