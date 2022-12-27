2db28bae3747b9a3cf09cd84ca41359c9bdc7784
jih3508 <whdlrgus32@naver.com>
2022-12-27 23:57:22 +0900

    余り (Remainder)

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER24078.java
@@ -0,0 +1,14 @@
+package BRONZEⅤ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER24078 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n % 21);
+	}
+}
