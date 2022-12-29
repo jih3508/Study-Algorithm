cd4c5785bfd7295a33df4a78a6a0bf27714593f4
jih3508 <whdlrgus32@naver.com>
2022-12-30 03:02:20 +0900

    РАВЕНСТВО

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER24309.java
@@ -0,0 +1,18 @@
+package BRONZEⅤ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class NUMBER24309 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger A = new BigInteger(br.readLine());
+		BigInteger B = new BigInteger(br.readLine());
+		BigInteger C = new BigInteger(br.readLine());
+		System.out.println((B.subtract(C)).divide(A));
+		
+	}
+}
