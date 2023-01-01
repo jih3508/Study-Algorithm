c020628de2d63a9992ae536ab05d69b358e9f9f2
jih3508 <whdlrgus32@naver.com>
2023-01-02 01:42:02 +0900

    UCPC에서 가장 쉬운 문제 번호는?

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER25311.java
@@ -0,0 +1,16 @@
+package BRONZEⅤ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER25311 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int y = Integer.parseInt(br.readLine());
+		System.out.println("A");
+	}
+
+}
