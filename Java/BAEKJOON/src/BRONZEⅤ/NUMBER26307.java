2ab83e329d058488c6c10559cc52f270fdd036b8
jih3508 <whdlrgus32@naver.com>
2023-01-10 01:47:45 +0900

    Correct

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER26307.java
@@ -0,0 +1,18 @@
+package BRONZEⅤ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NUMBER26307 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		System.out.println((h - 9) * 60 + m);
+	}
+}
