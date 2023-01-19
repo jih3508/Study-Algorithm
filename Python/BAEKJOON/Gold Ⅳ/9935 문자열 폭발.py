a6eee3a92213292e7695722464a4a5e8fca682ac
jih3508 <jih3508@ibizsoftware.net>
2023-01-19 17:00:37 +0900

    문자열 폭발

--- Python/BAEKJOON/Gold Ⅳ/9935 문자열 폭발.py
+++ Python/BAEKJOON/Gold Ⅳ/9935 문자열 폭발.py
@@ -1,12 +1,12 @@
 string = input()
 explore_string = input()
 lenth = len(explore_string)
-while string.count(explore_string):
-    stack = []
-    for s in string:
-        stack.append(s)
-        if len(s) <= len(explore_string) and ''.join(stack[-lenth:]) == explore_string:
-            for _ in range(lenth):
-                stack.pop()
-    string = ''.join(stack)
+stack = []
+for s in string:
+    stack.append(s)
+    if len(s) <= len(explore_string) and ''.join(stack[-lenth:]) == explore_string:
+        for _ in range(lenth):
+            stack.pop()
+string = ''.join(stack)
+    
 print(string if string else "FRULA")
