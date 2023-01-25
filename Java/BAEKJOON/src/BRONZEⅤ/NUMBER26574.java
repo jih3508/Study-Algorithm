0d94272ffe59c41e728e9b95f605c45d3643bceb
jih3508 <whdlrgus32@naver.com>
2023-01-26 00:02:16 +0900

    Copier

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER26574.java
@@ -0,0 +1,20 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER26574 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int x;
+		for(int i = 0; i < n; i++) {
+			x = Integer.parseInt(br.readLine());
+			System.out.println(x + "" + x);
+			
+		}
+	}
+}
