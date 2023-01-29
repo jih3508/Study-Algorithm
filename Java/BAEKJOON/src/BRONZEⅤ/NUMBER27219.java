0d2208b777461e623ca136cc4846f49c24bb7f33
jih3508 <whdlrgus32@naver.com>
2023-01-30 00:45:51 +0900

    Робинзон Крузо

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER27219.java
@@ -0,0 +1,23 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER27219 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N /5; i++) {
+			sb.append("V");
+		}
+		for(int i = 0; i < N % 5; i++) {
+			sb.append("I");
+		}
+		System.out.println(sb);
+	}
+
+}
