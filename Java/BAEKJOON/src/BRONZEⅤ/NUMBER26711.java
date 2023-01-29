c351600d787fb5ad2b99ea5af94c7dede8fd6aa9
jih3508 <whdlrgus32@naver.com>
2023-01-29 23:57:40 +0900

    A+B

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER26711.java
@@ -0,0 +1,17 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class NUMBER26711 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger A = new BigInteger(br.readLine());
+		System.out.println(A.add(new BigInteger(br.readLine())));		
+	}
+
+}
