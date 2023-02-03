a7a79f24a9103bf7e85acd23147a84a88e19b4b5
jih3508 <whdlrgus32@naver.com>
2023-02-03 23:54:39 +0900

    時間 (Hour)

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER27327.java
@@ -0,0 +1,15 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER27327 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(N * 24);
+	}
+}
