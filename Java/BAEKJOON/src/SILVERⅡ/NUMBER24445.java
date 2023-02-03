e27b9e48d4e923b4c5e0bf25a45b4da17ba41acd
jih3508 <jih3508@ibizsoftware.net>
2023-02-03 10:02:47 +0900

    알고리즘 수업 - 너비 우선 탐색 2

---
+++ Java/BAEKJOON/src/SILVERⅡ/NUMBER24445.java
@@ -0,0 +1,71 @@
+package SILVERⅡ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class NUMBER24445 {
+	
+	static Map<Integer, ArrayList<Integer>> grape = new HashMap<Integer, ArrayList<Integer>>();
+	static int N;
+	static int[] visited;
+	static int count = 1;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+		
+		for(int i = 1; i <= N; i++) {
+			grape.put(i, new ArrayList<Integer>());
+		}
+		visited = new int[N + 1];
+		
+		int a, b;
+		for(int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			grape.get(a).add(b);
+			grape.get(b).add(a);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			Collections.sort(grape.get(i), Collections.reverseOrder());
+		}
+		bfs(start);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= N; i++) {
+			sb.append(visited[i]).append("\n");
+		}
+		System.out.println(sb);
+		
+	}
+	
+	public static void bfs(int strat_node) {
+		visited[strat_node] = count;
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(strat_node);
+		int node;
+		while(!queue.isEmpty()) {
+			node = queue.poll();
+			for(int n : grape.get(node)) {
+				if(visited[n] == 0) {
+					visited[n] = ++count;
+					queue.add(n);
+				}
+			}
+		}
+	}
+
+}
