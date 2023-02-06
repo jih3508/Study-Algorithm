16a759d21d95613522198e60c7d954e58548b547
jih3508 <whdlrgus32@naver.com>
2023-02-06 23:41:52 +0900

    三方比較 (Three-Way Comparison)

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER27328.java
@@ -0,0 +1,24 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER27328 {
+	
+public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		if(A < B) {
+			System.out.println(-1);
+		}else if(A == B) {
+			System.out.println(0);
+		}else {
+			System.out.println(1);
+		}
+	}
+
+}
