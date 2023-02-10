56257715727a54d29b7f33e1c48ed8b683d9dbcd
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2023-02-10 23:55:12 +0900

    Metronome

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER27389.java
@@ -0,0 +1,16 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NUMBER27389 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		float N = Float.parseFloat(br.readLine());
+		System.out.printf("%.2f", N / 4);
+		
+	}
+
+}
