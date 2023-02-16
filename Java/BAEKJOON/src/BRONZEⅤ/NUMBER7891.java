8d12473a6e19bc8315ee09b89115f026a1ffe0e5
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2023-02-16 22:35:20 +0900

    Can you add this?

---
+++ Java/BAEKJOON/src/BRONZEⅤ/NUMBER7891.java
@@ -0,0 +1,24 @@
+package BRONZE¥´;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class NUMBER7891 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BigInteger a;
+		StringTokenizer st;
+		while(N-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			a = new BigInteger(st.nextToken());
+			System.out.println(a.add(new BigInteger(st.nextToken())));
+		}
+
+	}
+
+}
