86b76c52ed17ac0183c6381e1dfe80a9d45d5f28
로컬pc\회사pc <whdlrgus32@naver.com>
2026-09-15 17:59:17 +0900

    1683. Invalid Tweets

---
+++ SQL/LeetCode/easy/1683. Invalid Tweets.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Tweets (
+    tweet_id INT PRIMARY KEY,
+    content  VARCHAR(255)
+);
+
+INSERT INTO Tweets (tweet_id, content) VALUES
+(1, 'Let us Code'),
+(2, 'More than fifteen chars are here!');
+
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
+
+
+DROP TABLE  Tweets;
